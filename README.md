153607d115687195d95beb923d595a970c6a0501
aryan <aryandangar7@gmail.com>
2021-03-26 12:28:58 +0530

    2nd commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MyCodeEditor-Using-Python
- 
+>### Module Needed:
+1.  Tkinter 
+
+>### things to you can do 
+-  Write a python code 
+-  Save a python code 
+-  Execute a python code 
+-  Open any python code in editor
+
+>### Language
+     python 
+    
--- main.py
+++ main.py
@@ -3,12 +3,22 @@ from tkinter.filedialog import asksaveasfilename,askopenfilename
 
 compiler =Tk()
 compiler.title('my codeEditor ')
+file_path = ''
+
+def set_file_path(path):
+    global file_path
+    file_path =path
+
 
 def Save_as():
-    path= asksaveasfilename(filetypes=[('Python Files','*.py')])
+    if file_path == '':
+        path= asksaveasfilename(filetypes=[('Python Files','*.py')])
+    else:
+        path = file_path
     with open(path,'w')as file:
         code = editor.get('1.0',END)
         file.write(code)
+        set_file_path(path)
 
 def open_file():
     path= askopenfilename(filetypes=[('Python Files','*.py')])
@@ -16,6 +26,7 @@ def open_file():
         code =file.read()
         editor.delete('1.0',END)
         editor.insert('1.0',code)
+        set_file_path(path)
 
 
 def run():
@@ -27,7 +38,7 @@ menu_bar= Menu(compiler)
 
 file_menu= Menu(menu_bar,tearoff=0)
 file_menu.add_command(label='Open',command=open_file)
-file_menu.add_command(label='Save',command=run)
+file_menu.add_command(label='Save',command=Save_as)
 file_menu.add_command(label='Save AS',command=Save_as)
 file_menu.add_command(label='Exit',command=exit)
 menu_bar.add_cascade(label='File', menu=file_menu)
@@ -41,4 +52,6 @@ compiler.config(menu=menu_bar)
 
 editor = Text()
 editor.pack()
+
+
 compiler.mainloop()
